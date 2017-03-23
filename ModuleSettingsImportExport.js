23259a968ca1dc5b707e4eac622d7355de489d26
adrianbj <adrianbjones@gmail.com>
2017-03-23 11:28:01 -0700

    Add new batch export/import functionality to module's setting page.

--- ModuleSettingsImportExport.js
+++ ModuleSettingsImportExport.js
@@ -1,5 +1,30 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+
     $("#Inputfield_moduleSettings").on("focus", function () {
        $(this).select();
     });
+
+    $("#Inputfield_allModuleSettings").on("focus", function () {
+       $(this).select();
+    });
+
+	var allModuleSettings = ProcessWire.config ? ProcessWire.config.allModuleSettings : config.allModuleSettings;
+
+    $("input[name='selectedModules[]']").on('change', function() {
+
+    	var settingsJson = {};
+
+		$("input[name='selectedModules[]']").each(function() {
+			if($(this).is(':checked')) {
+				settingsJson[$(this).val()] = {};
+				settingsJson[$(this).val()]['version'] = JSON.parse(allModuleSettings[$(this).val()])[$(this).val()]['version'];
+				settingsJson[$(this).val()]['settings'] = JSON.parse(allModuleSettings[$(this).val()])[$(this).val()]['settings'];
+			}
+		});
+
+		$('#Inputfield_allModuleSettings').val(JSON.stringify(settingsJson));
+
+    });
+
 });
+
